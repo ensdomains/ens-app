4803cef28f7c39a627bc86337ec337e654ad6d3e
Jeff Lau <jeff-lau@live.com>
2018-05-14 21:15:28 +0800

    Add Domain state components

---
+++ src/components/EthRegistrar/DomainInfoStates.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export const Open = ({ domainState }) => (
+  <div>{domainState.name} is avaialble!</div>
+)
+
+export const Auction = () => <div>Auction</div>
+
+export const Owned = () => <div />
+
+export const Forbidden = () => <div />
+
+export const Reveal = () => <div />
+
+export const NotYetAvailable = () => <div />
