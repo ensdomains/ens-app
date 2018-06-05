a5d081423e1efbb5344647918326d5f5fa8c2d7f
Jeff Lau <jeff-lau@live.com>
2018-06-05 15:28:17 +0800

    Cleaned up contract instantiation

--- src/api/subDomainRegistrar.js
+++ src/api/subDomainRegistrar.js
@@ -6,40 +6,30 @@ let subDomainRegistrars = {}
 const defaultAddress = '0x0b07463b30b302a98407d3e3df85ebc073b0dbd1'
 
 const getSubDomainRegistrar = async address => {
-  const { web3 } = await getWeb3()
-
-  if (subDomainRegistrars[address]) {
-    return subDomainRegistrars[address]
-  } else {
-    subDomainRegistrars[address] = web3.eth
+  async function instantiateContract(address) {
+    const { web3 } = await getWeb3()
+    return (subDomainRegistrars[address] = web3.eth
       .contract(subDomainRegistrarContract)
-      .at(address)
-    return subDomainRegistrars[address]
+      .at(address))
   }
 
-  // if (
-  //   address === undefined &&
-  //   !subDomainRegistrars[defaultAddress] === undefined
-  // ) {
-  //   console.log('first if')
-  //   subDomainRegistrars[defaultAddress] = web3.eth
-  //     .contract(subDomainRegistrarContract)
-  //     .at(defaultAddress)
-  //   return subDomainRegistrars[defaultAddress]
-  // }
+  if (address) {
+    if (subDomainRegistrars[address]) {
+      return subDomainRegistrars[address]
+    } else {
+      subDomainRegistrars[address] = await instantiateContract(address)
+      return subDomainRegistrars[address]
+    }
+  }
 
-  // if (address && subDomainRegistrars[address] === undefined) {
-  //   console.log('here in alternative address')
-  //   subDomainRegistrars[address] = web3.eth
-  //     .contract(subDomainRegistrarContract)
-  //     .at(address)
-  //   return subDomainRegistrars[address]
-  // } else {
-  //   console.log('second else')
-  //   return subDomainRegistrars[address]
-  // }
-
-  // return subDomainRegistrars[defaultAddress]
+  if (subDomainRegistrars[defaultAddress]) {
+    return subDomainRegistrars[defaultAddress]
+  } else {
+    subDomainRegistrars[defaultAddress] = await instantiateContract(
+      defaultAddress
+    )
+    return subDomainRegistrars[defaultAddress]
+  }
 }
 
 export const query = async (subdomain, label, address) => {
@@ -64,17 +54,17 @@ export const queryAll = async label => {
       return query(domain.name, label, domain.registrar)
     }
     console.log('outside if')
-    return query(domain.name, label, defaultAddress)
+    return query(domain.name, label)
   })
 }
 
-// async function test() {
-//   // const node = await query('gimmethe', 'awesome')
-//   // console.log(node)
-//   const nodes = await queryAll('helloooo12345')
-//   nodes.map(promise =>
-//     promise.then(node => node.forEach(e => console.log(e.toString())))
-//   )
-// }
+async function test() {
+  // const node = await query('gimmethe', 'awesome')
+  // console.log(node)
+  const nodes = await queryAll('helloooo12345')
+  nodes.map(promise =>
+    promise.then(node => node.forEach(e => console.log(e.toString())))
+  )
+}
 
-// test()
+test()
