9a881593264b497c988784254fd84e05d2f517cf
Jeff Lau <jeff-lau@live.com>
2018-07-18 18:02:26 +0100

    Added basic name validation for details page

--- src/routes/SingleName.js
+++ src/routes/SingleName.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import { validateName, parseSearchTerm } from '../lib/utils'
 
 class SingleName extends Component {
+  state = {
+    valid: false
+  }
+  componentDidMount() {
+    const searchTerm = this.props.match.params.name
+    const validity = parseSearchTerm(searchTerm)
+    if (parseSearchTerm(this.props.match.params.name) === 'eth') {
+      this.setState({
+        valid: true
+      })
+    } else {
+      this.setState({
+        valid: false
+      })
+    }
+  }
   render() {
-    console.log(this.props.match)
+    const searchTerm = this.props.match.params.name
+    if (this.state.valid) {
+      return <div>Details for {searchTerm}</div>
+    }
 
-    return <div>{this.props.match.params.name}</div>
+    return <div>Invalid domain name {searchTerm}</div>
   }
 }
 
