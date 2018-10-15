a845698244783c64d84e795e96db5b09f95179fd
Makoto <inouemak@googlemail.com>
2018-10-15 12:21:35 +0100

    Add surge deploy hook on traavis

--- src/components/SearchName/Search.js
+++ src/components/SearchName/Search.js
@@ -89,7 +89,7 @@ class Search extends React.Component {
         }}
       >
         <input
-          placeholder="Search names and addresses"
+          placeholder="Search names and addresses surge"
           ref={el => (this.input = el)}
           onChange={this.handleParse}
         />
