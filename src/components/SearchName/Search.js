3ad59e71c8b8af50e879e3caf7a8fc5b815f3c38
Makoto <inouemak@googlemail.com>
2018-10-16 16:44:35 +0100

    Increament to surge 3

--- src/components/SearchName/Search.js
+++ src/components/SearchName/Search.js
@@ -89,7 +89,7 @@ class Search extends React.Component {
         }}
       >
         <input
-          placeholder="Search names and addresses surge2"
+          placeholder="Search names and addresses surge3"
           ref={el => (this.input = el)}
           onChange={this.handleParse}
         />
