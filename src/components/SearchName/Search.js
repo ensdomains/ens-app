489ff825854ff78f9c1d96cb507e96a3761b31f8
Makoto <inouemak@googlemail.com>
2018-10-16 16:53:37 +0100

    Bump it to 4

--- src/components/SearchName/Search.js
+++ src/components/SearchName/Search.js
@@ -89,7 +89,7 @@ class Search extends React.Component {
         }}
       >
         <input
-          placeholder="Search names and addresses surge3"
+          placeholder="Search names and addresses surge4"
           ref={el => (this.input = el)}
           onChange={this.handleParse}
         />
