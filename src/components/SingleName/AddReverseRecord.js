f2bf38f5e03438463d39a8317afc118176d986ef
Jeff Lau <jeff-lau@live.com>
2018-11-05 17:34:42 +0000

    Add edit functionality with values ready to style

--- src/components/SingleName/AddReverseRecord.js
+++ src/components/SingleName/AddReverseRecord.js
@@ -1,31 +1,48 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
 import ReverseRecordQuery from '../ReverseRecordQuery'
+import Editable from './Editable'
+import SaveCancel from './SaveCancel'
 
 const AddReverseRecordContainer = styled('div')``
+const SetReverseContainer = styled('div')``
 
 class AddReverseRecord extends Component {
   render() {
     const { account, name } = this.props
     return (
       <AddReverseRecordContainer>
-        <ReverseRecordQuery address={account}>
-          {({ data: { getReverseRecord }, loading }) => {
-            if (loading) return 'loading'
-            console.log(name, getReverseRecord.name)
-            if (getReverseRecord) {
-              if (name === getReverseRecord.name) {
-                return `awesome you setup your reverse with ${name} already`
-              } else {
-                return `Reverse record is set to ${
-                  getReverseRecord.name
-                }, would you like to switch?`
-              }
-            } else {
-              return `reverse not set`
-            }
-          }}
-        </ReverseRecordQuery>
+        <Editable>
+          {({ editing, startEditing, stopEditing }) => (
+            <ReverseRecordQuery address={account}>
+              {({ data: { getReverseRecord }, loading }) => {
+                if (loading) return 'loading'
+                return (
+                  <>
+                    {getReverseRecord ? (
+                      name === getReverseRecord.name ? (
+                        `awesome you setup your reverse with ${name} already`
+                      ) : (
+                        <div onClick={editing ? stopEditing : startEditing}>
+                          {`your reverse is set to ${getReverseRecord.name} 
+                          already, would you like to change it?`}
+                        </div>
+                      )
+                    ) : (
+                      `reverse not set`
+                    )}
+                    {editing && (
+                      <SetReverseContainer>
+                        {account} - {name}
+                        <SaveCancel />
+                      </SetReverseContainer>
+                    )}
+                  </>
+                )
+              }}
+            </ReverseRecordQuery>
+          )}
+        </Editable>
       </AddReverseRecordContainer>
     )
   }
