a52e1b547fef756873f47d126ea726872f4d35ba
Jeff Lau <jeff-lau@live.com>
2018-12-17 13:50:03 +0000

    Mobile styles for reverse record

--- src/components/SingleName/DetailsItem.js
+++ src/components/SingleName/DetailsItem.js
@@ -36,7 +36,7 @@ export const DetailsKey = styled('div')`
 `
 
 export const DetailsValue = styled('div')`
-  font-size: 16px;
+  font-size: 14px;
   font-weight: 100;
   font-family: Overpass Mono;
   white-space: nowrap;
