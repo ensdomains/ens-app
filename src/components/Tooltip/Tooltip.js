9a9afbc3c4742707705535fe06b34f69492475d4
Makoto <inouemak@googlemail.com>
2019-01-29 13:34:57 +0000

    Change tooltip to use hooks

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -4,72 +4,53 @@ import { findDOMNode } from 'react-dom'
 import uuid from 'uuid'
 import ReactTooltip from 'react-tooltip'
 import styled from 'react-emotion'
+import { useState, useEffect } from 'react'
 
 const DefaultTooltip = styled(ReactTooltip)`
   box-shadow: -4px 18px 70px 0 rgba(108, 143, 167, 0.32);
   opacity:1 !important;
 `
 
-export default class Tooltip extends Component {
-  static propTypes = {
-    children: PropTypes.func.isRequired
-  }
+const TooltipContainer = (props) => {
+  const { text, position, children } = props
+  const id = uuid()
+  const [show, setShow] = useState(false)
+  const tooltipRef = React.createRef()
 
-  state = {}
-
-  componentDidMount() {
-    this.id = uuid()
-    this.componentDidUpdate()
-  }
-
-  componentDidUpdate() {
-    if (!this.elem) {
-      return
-    }
-
-    if (this.state.show) {
-      ReactTooltip.show(findDOMNode(this.elem))
+  useEffect(() => {
+    if (show) {
+      ReactTooltip.show(findDOMNode(tooltipRef.current))
     } else {
-      ReactTooltip.hide(findDOMNode(this.elem))
+      ReactTooltip.hide(findDOMNode(tooltipRef.current))
     }
-  }
+  })
 
-  _onRef = e => {
-    this.elem = e
-  }
+  const tooltipElement = (
+    <span data-tip={text} data-for={id} ref={tooltipRef} />
+  )
 
-  render() {
-    const { text, position, children } = this.props
-
-    return (
-      <>
-        {children({
-          showTooltip: this.show,
-          hideTooltip: this.hide,
-          tooltipElement: (
-            <span data-tip={text} data-for={this.id} ref={this._onRef} />
-          )
-        })}
-        <DefaultTooltip
-          data-testid='tooltip'
-          id={this.id}
-          event="dbclick"
-          place={position || 'top'}
-          effect="solid"
-          type="light"
-          html={true}
-        >
-        </DefaultTooltip>
-      </>
-    )
-  }
-
-  show = () => {
-    this.setState({ show: true })
-  }
-
-  hide = () => {
-    this.setState({ show: false })
-  }
+  return (
+    <>
+      {children({
+        showTooltip: () => { setShow(true) } ,
+        hideTooltip: () => { setShow(false) } ,
+        tooltipElement: tooltipElement
+      })}
+      <DefaultTooltip
+        data-testid='tooltip'
+        id={id}
+        event="dbclick"
+        place={position || 'top'}
+        effect="solid"
+        type="light"
+        html={true}
+      >
+      </DefaultTooltip>
+    </>
+  )
 }
 
+TooltipContainer.propTypes = {
+  children: PropTypes.func.isRequired
+}
+export default TooltipContainer
