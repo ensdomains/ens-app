7a4e0ae9a30cef823b2ac80096dc1a2469a31975
Jeff Lau <jeff-lau@live.com>
2019-02-18 14:09:52 +0800

    Convert component to function

--- src/routes/SingleName.js
+++ src/routes/SingleName.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { GET_SINGLE_NAME } from '../graphql/queries'
 import { Query } from 'react-apollo'
 import Loader from '../components/Loader'
 
 import Name from '../components/SingleName/Name'
 
-class SingleName extends Component {
-  render() {
-    const {
-      match: {
-        params: { name: searchTerm }
-      },
-      location: { pathname }
-    } = this.props
-    return (
-      <Query query={GET_SINGLE_NAME} variables={{ name: searchTerm }}>
-        {({ loading, error, data, refetch }) => {
-          if (loading) return <Loader large center />
-          if (error)
-            return <div>{(console.log(error), JSON.stringify(error))}</div>
-          return (
-            <Name
-              details={data.singleName}
-              name={searchTerm}
-              pathname={pathname}
-              refetch={refetch}
-            />
-          )
-        }}
-      </Query>
-    )
-  }
+function SingleName({
+  match: {
+    params: { name: searchTerm }
+  },
+  location: { pathname }
+}) {
+  return (
+    <Query query={GET_SINGLE_NAME} variables={{ name: searchTerm }}>
+      {({ loading, error, data, refetch }) => {
+        if (loading) return <Loader large center />
+        if (error)
+          return <div>{(console.log(error), JSON.stringify(error))}</div>
+        return (
+          <Name
+            details={data.singleName}
+            name={searchTerm}
+            pathname={pathname}
+            refetch={refetch}
+          />
+        )
+      }}
+    </Query>
+  )
 }
 
 export default SingleName
