cbf4316f7b7fa3d9b20ac8f09b6e5177e1444a1a
Jeff Lau <jeff-lau@live.com>
2019-03-25 17:50:56 +0800

    Add two more obvious test cases

--- cypress/integration/registrar.spec.js
+++ cypress/integration/registrar.spec.js
@@ -2,8 +2,8 @@ const ROOT = Cypress.env('ROOT')
 const NAME_ROOT = Cypress.env('NAME_ROOT')
 
 describe('PermanentRegistrar', () => {
-  it('can visit a name', () => {
-    cy.visit(`${ROOT}/name/vitalik3.eth`)
+  it('can visit a name and register it', () => {
+    cy.visit(`${ROOT}/name/vitalik.eth`)
     cy.getByTestId('request-register-button').click()
     cy.getByTestId('register-button').click()
     cy.getByTestId('manage-name-button').click()
@@ -12,5 +12,19 @@ describe('PermanentRegistrar', () => {
       'have.text',
       Cypress.env('ownerAddress')
     )
+  })
+
+  it('cannot register a name that is already owned', () => {
+    cy.visit(`${ROOT}/name/vitalik.eth`)
+    cy.getByTestId('details-value-owner', { exact: false }).should(
+      'have.text',
+      Cypress.env('ownerAddress')
+    )
+  })
+
+  it('can visit a name and cannot register before the wait time is over', () => {
+    cy.visit(`${ROOT}/name/vitalik2.eth`)
+    cy.getByTestId('request-register-button').click()
+    cy.queryByText('register-button').should('not.exist')
   })
 })
