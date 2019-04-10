13f0a05179c95a74ae4fd6643cdf62586bd40e19
Jeff Lau <tyranel@hotmail.com>
2019-04-10 14:36:15 +0800

    Fix warnings (#161)

--- src/components/SingleName/DetailsItemEditable.js
+++ src/components/SingleName/DetailsItemEditable.js
@@ -268,12 +268,12 @@ const Editable = ({
 
 class DetailsEditable extends Component {
   _renderViewOnly() {
-    let { value, keyName, type, isOwner, deedOwner, isDeedOwner } = this.props
+    let { value, keyName, type, deedOwner, isDeedOwner } = this.props
     if (parseInt(value, 16) === 0) {
       let [newValue, newType] = getDefaultMessage(keyName)
       value = newValue
       type = newType
-      if(keyName == 'Owner' && parseInt(deedOwner, 16) !== 0){
+      if(keyName === 'Owner' && parseInt(deedOwner, 16) !== 0){
         value = 'Pending'
         if(isDeedOwner){
           value+='(You have not finalised)'
