d95fdab42fa5ccbd890b414816d5db2535e4f9ec
Jeff Lau <jeff-lau@live.com>
2019-04-17 18:22:35 +0800

    use parseFloat instead of parseInt

--- src/components/SingleName/NameRegister/NameRegister.js
+++ src/components/SingleName/NameRegister/NameRegister.js
@@ -67,7 +67,7 @@ const NameRegister = ({ domain, waitTime, refetch }) => {
     timerRunning ? 1000 : null
   )
 
-  const duration = 31556952 * parseInt(years)
+  const duration = 31556952 * parseFloat(years)
   const waitPercentComplete = (secondsPassed / waitTime) * 100
 
   return (
