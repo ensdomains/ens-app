f18ef820928da8ee6ab71ec9b5bb9a2f8377964d
Makoto Inoue <2630+makoto@users.noreply.github.com>
2019-04-25 05:27:31 +0100

    Show error when short name eth address is specified under /name (#202)
    * Show error when short name eth address is specified uner /name
    
    * prettify

--- src/routes/SingleName.js
+++ src/routes/SingleName.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { validateName } from '../utils/utils'
+import { validateName, parseSearchTerm } from '../utils/utils'
 import { GET_SINGLE_NAME } from '../graphql/queries'
 import { Query } from 'react-apollo'
 import Loader from '../components/Loader'
@@ -14,19 +14,30 @@ function SingleName({
   location: { pathname }
 }) {
   const [valid, setValid] = useState(undefined)
+  const [type, setType] = useState(undefined)
   const [name, setNormalisedName] = useState('')
+  let normalisedName, errorMessage, _type
 
   useEffect(() => {
     try {
       // This is under the assumption that validateName never returns false
-      const normalisedName = validateName(searchTerm)
-      setValid(true)
+      normalisedName = validateName(searchTerm)
       setNormalisedName(normalisedName)
-
       document.title = searchTerm
     } catch {
-      setValid(false)
       document.title = 'Error finding name'
+    } finally {
+      if (normalisedName) {
+        _type = parseSearchTerm(normalisedName)
+      } else {
+        _type = parseSearchTerm(searchTerm)
+      }
+      setType(_type)
+      if (_type === 'supported') {
+        setValid(true)
+      } else {
+        setValid(false)
+      }
     }
   }, [searchTerm])
 
@@ -49,7 +60,19 @@ function SingleName({
       </Query>
     )
   } else if (valid === false) {
-    return <SearchErrors errors={['domainMalformed']} searchTerm={searchTerm} />
+    if (type === 'invalid') {
+      errorMessage = 'domainMalformed'
+    } else if (type === 'short') {
+      errorMessage = 'tooShort'
+    } else {
+      errorMessage = type
+    }
+    return (
+      <SearchErrors
+        errors={[errorMessage]}
+        searchTerm={normalisedName || searchTerm}
+      />
+    )
   } else {
     return <Loader large center />
   }
