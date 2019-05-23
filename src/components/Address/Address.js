331bcf6a041c289835cf5e097788f02125d56b1e
ConorOkus <conor.okus@hotmail.co.uk>
2019-05-23 22:26:16 +0100

    Debounce search term and resolve name after user finishes typing

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -1,32 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import ENS from 'ethereum-ens'
+
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value])
+
+  return debouncedValue
+}
+
+function resolveName(search, provider) {
+  const ens = new ENS(provider)
+
+  return ens
+    .resolver(search)
+    .addr()
+    .then(addr => addr)
+    .catch(err => {
+      console.error(err)
+      return 'Unable to find address'
+    })
+}
 
 const AddressInput = ({ provider }) => {
   const [inputValue, setInputValue] = useState('')
   const [resolvedAddress, setResolvedAddress] = useState('')
+  const [isResolving, setIsResolving] = useState(false)
+
+  const debouncedSearchTerm = useDebounce(inputValue, 500)
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      setIsResolving(true)
+
+      resolveName(debouncedSearchTerm, provider).then(result => {
+        setIsResolving(false)
+
+        setResolvedAddress(result)
+      })
+    } else {
+      setIsResolving(false)
+    }
+  }, [debouncedSearchTerm])
 
   return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault()
-
-        const ens = new ENS(provider)
-        const address = await ens.resolver(inputValue).addr()
-
-        setResolvedAddress(address)
-
-        return false
-      }}
-    >
+    <>
+      <label>Search ENS name </label>
       <input
-        onChange={e => {
+        onChange={async e => {
           setInputValue(e.currentTarget.value)
         }}
       />
+      {isResolving && <p>Searching ...</p>}
       <p>{resolvedAddress}</p>
-      <button type="submit">Search</button>
-    </form>
+    </>
   )
 }
 
