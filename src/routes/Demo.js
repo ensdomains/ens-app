5a9520931e231c067e74bc2568c2f9ccbf07817e
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-05-24 01:00:09 +0100

    Remove styles

--- src/routes/Demo.js
+++ src/routes/Demo.js
@@ -15,32 +15,14 @@ const AutoComplete = styled.div`
   display: flex;
   flex-direction: column;
   background-color: #ffffff;
+  position: relative;
+  z-index: 10000;
+  height: 90px;
+
   ${mq.medium`
       width: calc(100% - 162px);
       font-size: 28px;
   `}
-
-  .address {
-    font-size: 18px;
-    padding: 0 0 0 40px;
-    font-family: Overpass;
-    font-weight: 100;
-    color: rgb(202, 202, 202);
-  }
-  .error {
-    color: #cc0000;
-    font-size: 18px;
-    padding: 0 0 0 40px;
-    font-family: Overpass;
-    font-weight: 100;
-  }
-`
-
-const AddressForm = styled('form')`
-  display: flex;
-  position: relative;
-  z-index: 10000;
-
   input {
     padding: 20px 0 0 40px;
     width: 100%;
@@ -63,35 +45,30 @@ const AddressForm = styled('form')`
     }
   }
 
-  button {
-    background: #5284ff;
-    color: white;
-    font-size: 22px;
+  .address,
+  .searching {
+    font-size: 18px;
+    padding: 0 0 0 40px;
     font-family: Overpass;
-    padding: 20px 0;
-    height: 90px;
-    width: 162px;
-    border: none;
-    display: none;
-    ${mq.medium`
-      display: block;
-    `}
-
-    &:hover {
-      cursor: pointer;
-    }
+    font-weight: 100;
+  }
+  .error {
+    color: #cc0000;
+    font-size: 18px;
+    padding: 0 0 0 40px;
+    font-family: Overpass;
+    font-weight: 100;
   }
 `
 
-const AddressStyle = {
+const ClassName = {
   Blockies,
-  AutoComplete,
-  AddressForm
+  AutoComplete
 }
 
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8546', null, {})
 
 const DemoView = () => (
-  <Address className={AddressStyle} provider={web3.givenProvider} />
+  <Address className={ClassName} provider={web3.givenProvider} />
 )
 export default DemoView
