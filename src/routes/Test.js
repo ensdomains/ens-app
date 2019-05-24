7922b7c97387ba01ee1a1f07f311f607f4927380
Adetayo Timilehin <timilehin1999@gmail.com>
2019-05-24 14:36:46 +0100

    Update Test.js

--- src/routes/Test.js
+++ src/routes/Test.js
@@ -5,5 +5,10 @@ import AddressInput from '../components/Address/Address'
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8546', null, {})
 
 export const TestPage = () => {
-  return <AddressInput provider={web3.givenProvider} className={{}} />
+  return (
+    <AddressInput
+      provider={web3.givenProvider}
+      classNames={{ label: '', input: '' }}
+    />
+  )
 }
