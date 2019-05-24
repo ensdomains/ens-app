7883e71b70e9c2ce3343d02b6543f460808d2b28
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-05-24 07:24:55 +0100

    Add prop types definition

--- src/components/Address/index.js
+++ src/components/Address/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import ENS from 'ethereum-ens'
 
 function useDebounce(value, timeout) {
@@ -108,6 +109,17 @@ function Address({ className, provider, onComplete, placeholder }) {
       )}
     </AutoComplete>
   )
+}
+
+Address.propTypes = {
+  className: PropTypes.object.isRequired,
+  provider: PropTypes.object.isRequired,
+  onComplete: PropTypes.func.isRequired,
+  placeholder: PropTypes.string
+}
+
+Address.defaultProps = {
+  placeholder: 'Enter Address or ENS Name'
 }
 
 export default Address
