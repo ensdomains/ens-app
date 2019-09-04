57e66e0eac21c81639ae36cce8fa4be6b68e2ae5
Jeff Lau <jeff-lau@live.com>
2019-09-04 15:49:02 +0800

    WIP

--- cypress/integration/favorites.spec.js
+++ cypress/integration/favorites.spec.js
@@ -4,21 +4,27 @@ const NAME_ROOT = Cypress.env('NAME_ROOT')
 describe('Favorites', () => {
   it('can add and remove favourites', () => {
     cy.visit(`${ROOT}/favourites`)
-    cy.queryByText('No names have been saved.', { exact: false }).should('exist')
-  
+    cy.queryByText('No names have been saved.', { exact: false }).should(
+      'exist'
+    )
+
     cy.visit(`${NAME_ROOT}/resolver.eth`)
-    cy.getByTestId('add-favorite').click()
-  
+    cy.getByTestId('add-favorite').click({ force: true })
+
     cy.visit(`${ROOT}/favourites`)
-    cy.queryByText('No names have been saved.', { exact: false }).should('not.exist')
+    cy.queryByText('No names have been saved.', { exact: false }).should(
+      'not.exist'
+    )
     cy.getByTestId('favourites-container').within(container => {
       cy.queryByText('resolver.eth', { exact: false }).should('exist')
     })
 
     cy.visit(`${NAME_ROOT}/resolver.eth`)
-    cy.getByTestId('add-favorite').click()
-  
+    cy.getByTestId('add-favorite').click({ force: true })
+
     cy.visit(`${ROOT}/favourites`)
-    cy.queryByText('No names have been saved.', { exact: false }).should('exist')
+    cy.queryByText('No names have been saved.', { exact: false }).should(
+      'exist'
+    )
   })
 })
