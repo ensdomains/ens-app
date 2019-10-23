222f2ac82a72add17db86819cdf236388d6883db
Makoto <inouemak@googlemail.com>
2019-10-23 14:57:26 +0100

    Add validation on each key

--- src/components/SingleName/Address/Address.js
+++ src/components/SingleName/Address/Address.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { COIN_LIST } from './constants'
 import KeyValueRecord from '../KeyValueRecord'
+import { formatsByName } from '@ensdomains/address-encoder'
+
+const validator = (symbol, value) => {
+  let isValid = true
+  try {
+    formatsByName[symbol].decoder(value)
+  } catch {
+    isValid = false
+  }
+  return isValid
+}
 
 export default function Address(props) {
-  return <KeyValueRecord {...props} keys={COIN_LIST} />
+  return <KeyValueRecord {...props} keys={COIN_LIST} validator={validator} />
 }
