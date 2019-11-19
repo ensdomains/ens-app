406a861c17da1f12a3ebd729496a4bbf41a67ca1
Jeff Lau <tyranel@hotmail.com>
2019-11-19 18:06:57 +0800

    Split signer (#533)
    * Remove reload on accountsChange. Update ui to add split signer
    
    * Update lock file
    
    * WIP changed reloadOnAccountsChange to true
    
    * Change reload back to false
    
    * Add waiting message to network info query
    
    * update UI package
    
    * Add timeouts for cypress tests
    
    * Update UI package
    
    * Revert spec changes
    
    * Update UI package

--- src/components/NetworkInformation/NetworkInfoQuery.js
+++ src/components/NetworkInformation/NetworkInfoQuery.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import Loader from '../Loader'
@@ -12,10 +13,29 @@ export const GET_WEB3 = gql`
   }
 `
 
+const Waiting = styled('div')`
+  color: #ccc;
+  display: flex;
+  font-size: 11px;
+  text-transform: uppercase;
+  font-weight: 700;
+`
+
+const WaitingText = styled('span')`
+  margin-right: 5px;
+`
+
 const NetworkInfoQuery = ({ noLoader, children }) => (
   <Query query={GET_WEB3}>
     {({ data, loading, error }) => {
-      if (loading) return noLoader ? '' : <Loader />
+      if (loading)
+        return noLoader ? (
+          ''
+        ) : (
+          <Waiting>
+            <WaitingText>Waiting for accounts</WaitingText> <Loader />
+          </Waiting>
+        )
       const {
         web3: { accounts, network }
       } = data
