bac9711e7d082b80b5a63113ef28d8455030e46a
Makoto Inoue <makoto@Makotos-iMac.lan>
2020-03-16 17:29:51 +0000

    Use sub1 label for testing

--- src/index.js
+++ src/index.js
@@ -24,17 +24,14 @@ window.addEventListener('load', async () => {
         customProvider: 'http://localhost:8545',
         ensAddress: process.env.REACT_APP_ENS_ADDRESS
       })
-      let labels = window.localStorage['labels'] ? JSON.parse(labelsJSON) : {}
-      // a test label for testing new subdomain
-      const testLabels = {
-        f66494dcc1f75a779961a5e1e042022f740297f85ecacf57ab91f3e099338a38: 'okay'
-      }
+      let labels = window.localStorage['labels']
+        ? JSON.parse(window.localStorage['labels'])
+        : {}
       window.localStorage.setItem(
         'labels',
         JSON.stringify({
           ...labels,
-          ...JSON.parse(process.env.REACT_APP_LABELS),
-          ...testLabels
+          ...JSON.parse(process.env.REACT_APP_LABELS)
         })
       )
     } else {
