7cbad77bffbcb56110a98deeccfe528009f0d55b
Makoto Inoue <makoto@Makotos-iMac.lan>
2020-03-16 11:52:43 +0000

    Disable storing test labels

--- src/index.js
+++ src/index.js
@@ -31,16 +31,16 @@ window.addEventListener('load', async () => {
       } else {
         labels = {}
       }
-      labels[
-        '03bd642dd09510c5661d684107f8c8616af5a619c17a899a90b36056efa59411'
-      ] = 'subdomaindummy'
-      labels[
-        '8cfce3846b2a51b94d2cc5646dc3b6249f78c82e2441b3d32e2a1e6feb93ee49'
-      ] = 'original'
-      labels[
-        'f66494dcc1f75a779961a5e1e042022f740297f85ecacf57ab91f3e099338a38'
-      ] = 'okay'
-      window.localStorage.setItem('labels', JSON.stringify(labels))
+      // labels[
+      //   '03bd642dd09510c5661d684107f8c8616af5a619c17a899a90b36056efa59411'
+      // ] = 'subdomaindummy'
+      // labels[
+      //   '8cfce3846b2a51b94d2cc5646dc3b6249f78c82e2441b3d32e2a1e6feb93ee49'
+      // ] = 'original'
+      // labels[
+      //   'f66494dcc1f75a779961a5e1e042022f740297f85ecacf57ab91f3e099338a38'
+      // ] = 'okay'
+      // window.localStorage.setItem('labels', JSON.stringify(labels))
     } else {
       await setup({
         reloadOnAccountsChange: false
