b918c2ffd7b426cca43bcb1e2d9ebc2217b51ee1
Jeff Lau <jeff-lau@live.com>
2020-03-25 15:03:09 +0800

    Add single name banner

--- src/components/SingleName/Name.js
+++ src/components/SingleName/Name.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 
 import { useMediaMin } from 'mediaQuery'
 import { EMPTY_ADDRESS } from '../../utils/records'
+import warning from '../../assets/yellowwarning.svg'
 
 import { Title } from '../Typography/Basic'
 import TopBar from '../Basic/TopBar'
@@ -22,6 +23,22 @@ const Owner = styled('div')`
 const RightBar = styled('div')`
   display: flex;
   align-items: center;
+`
+
+const RenewalReminder = styled('div')`
+  h3 {
+    margin: 0;
+    color: #f5a623;
+    font-weight: 300;
+    font-size: 18px;
+  }
+  border-radius: 5px;
+  color: #2b2b2b;
+  font-weight: 300;
+  font-size: 18px;
+  background: white;
+  padding: 20px;
+  margin-bottom: 20px;
 `
 
 const Favourite = styled(DefaultFavourite)``
@@ -78,62 +95,72 @@ function Name({ details: domain, name, pathname, type, refetch }) {
     containerState = isOwner ? 'Yours' : domain.state
   }
   return (
-    <NameContainer state={containerState}>
-      <TopBar percentDone={percentDone}>
-        <Title>
-          {domain.decrypted
-            ? name
-            : '[unknown' +
-              domain.name.split('.')[0].slice(1, 11) +
-              ']' +
-              '.' +
-              domain.parent}
-        </Title>
-        <RightBar>
-          {!!ownerType && <Owner data-testid="owner-type">{ownerType}</Owner>}
-          <Favourite domain={domain} />
-          {smallBP && (
-            <Tabs
-              pathname={pathname}
-              tab={preferredTab}
-              domain={domain}
-              parent={domain.parent}
-            />
-          )}
-        </RightBar>
-      </TopBar>
-      {!smallBP && (
-        <Tabs
-          pathname={pathname}
-          tab={preferredTab}
-          domain={domain}
-          parent={domain.parent}
-        />
-      )}
-      {isDNSRegistrationOpen(domain) ? (
-        <DNSNameRegister
-          domain={domain}
-          pathname={pathname}
-          refetch={refetch}
-          account={account}
-          readOnly={account === EMPTY_ADDRESS}
-        />
-      ) : type === 'short' && domain.owner === EMPTY_ADDRESS ? ( // check it's short and hasn't been claimed already
-        <ShortName name={name} />
-      ) : (
-        <NameDetails
-          tab={preferredTab}
-          domain={domain}
-          pathname={pathname}
-          name={name}
-          isOwner={isOwner}
-          isOwnerOfParent={isOwnerOfParent}
-          refetch={refetch}
-          account={account}
-          registrationOpen={registrationOpen}
-        />
-      )}
-    </NameContainer>
+    <>
+      <RenewalReminder>
+        <h3>
+          <img src={warning} />
+          &nbsp; Names in May are expiring soon
+        </h3>
+        Click the renew button to avoid expiration. To renew all of your names
+        in bulk, navigate to My Names
+      </RenewalReminder>
+      <NameContainer state={containerState}>
+        <TopBar percentDone={percentDone}>
+          <Title>
+            {domain.decrypted
+              ? name
+              : '[unknown' +
+                domain.name.split('.')[0].slice(1, 11) +
+                ']' +
+                '.' +
+                domain.parent}
+          </Title>
+          <RightBar>
+            {!!ownerType && <Owner data-testid="owner-type">{ownerType}</Owner>}
+            <Favourite domain={domain} />
+            {smallBP && (
+              <Tabs
+                pathname={pathname}
+                tab={preferredTab}
+                domain={domain}
+                parent={domain.parent}
+              />
+            )}
+          </RightBar>
+        </TopBar>
+        {!smallBP && (
+          <Tabs
+            pathname={pathname}
+            tab={preferredTab}
+            domain={domain}
+            parent={domain.parent}
+          />
+        )}
+        {isDNSRegistrationOpen(domain) ? (
+          <DNSNameRegister
+            domain={domain}
+            pathname={pathname}
+            refetch={refetch}
+            account={account}
+            readOnly={account === EMPTY_ADDRESS}
+          />
+        ) : type === 'short' && domain.owner === EMPTY_ADDRESS ? ( // check it's short and hasn't been claimed already
+          <ShortName name={name} />
+        ) : (
+          <NameDetails
+            tab={preferredTab}
+            domain={domain}
+            pathname={pathname}
+            name={name}
+            isOwner={isOwner}
+            isOwnerOfParent={isOwnerOfParent}
+            refetch={refetch}
+            account={account}
+            registrationOpen={registrationOpen}
+          />
+        )}
+      </NameContainer>
+    </>
   )
 }
 
