c7af5e1d72c621bf9f54e02a1452ae3a82f92c18
Jeff Lau <jeff-lau@live.com>
2020-04-01 18:50:34 +0800

    use url to control registrant/controller filter

--- src/routes/AddressPage.js
+++ src/routes/AddressPage.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Address from '../components/Address'
 
+const stripTrailingSlash = str => {
+  return str.endsWith('/') ? str.slice(0, -1) : str
+}
+
 const AddressContainer = ({ match, location }) => {
   const address = match.params.address.toLowerCase()
+  const domainType = match.params.domainType.toLowerCase()
   const queryParams = new URLSearchParams(location.search)
   return (
     <Address
+      url={stripTrailingSlash(match.url)}
       address={address}
+      domainType={domainType}
       showOriginBanner={queryParams.get('origin') === 'renew'}
     />
   )
