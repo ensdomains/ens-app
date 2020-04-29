c28755a4edc57e036c3b6f8186826c2a5d54b5ae
Makoto <inouemak@googlemail.com>
2020-04-29 21:58:03 +0100

    Revert "Add avatar everywhere"
    This reverts commit d8b3ef6d380cc29cca91456a81c16655871d9e4f.

--- src/components/Blockies.js
+++ src/components/Blockies.js
@@ -2,13 +2,7 @@ import React from 'react'
 import createIcon from '../utils/blockies'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { useQuery } from 'react-apollo'
-import { GET_REVERSE_RECORD } from '../graphql/queries'
-const Avatar = styled('img')`
-  border-radius: 50%;
-  box-shadow: 2px 2px 9px 0 #e1e1e1;
-  flex-shrink: 0;
-`
+
 const BlockiesContainer = styled('span')``
 
 const Blockies = ({
@@ -19,27 +13,14 @@ const Blockies = ({
   spotcolor,
   className
 }) => {
-  const {
-    data: { getReverseRecord } = {},
-    loading: reverseRecordLoading
-  } = useQuery(GET_REVERSE_RECORD, {
-    variables: {
-      address
-    }
-  })
-  let imgURL
-  if (getReverseRecord && getReverseRecord.avatar) {
-    imgURL = getReverseRecord.avatar
-  } else {
-    imgURL = createIcon({
-      seed: address.toLowerCase(),
-      size: 8,
-      scale: 5,
-      color,
-      bgcolor,
-      spotcolor
-    }).toDataURL()
-  }
+  var imgURL = createIcon({
+    seed: address.toLowerCase(),
+    size: 8,
+    scale: 5,
+    color,
+    bgcolor,
+    spotcolor
+  }).toDataURL()
   var style = {
     backgroundImage: 'url(' + imgURL + ')',
     backgroundSize: 'cover',
@@ -47,6 +28,7 @@ const Blockies = ({
     height: imageSize + 'px',
     display: 'inline-block'
   }
+
   return <BlockiesContainer className={className} style={style} />
 }
 
