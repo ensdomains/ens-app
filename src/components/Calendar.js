cd42c3dc9c4e56eda880908032bcacbef79247e1
Jeff Lau <jeff-lau@live.com>
2020-04-30 18:23:22 +0800

    Begin adding calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,18 +3,20 @@ import AddToCalendar from 'react-add-to-calendar'
 
 function CalendarInvite(
   startTime = '2016-09-16T20:15:00-04:00',
-  endTime = '2016-09-16T21:45:00-04:00'
+  endTime = '2016-09-16T21:45:00-04:00',
+  type = '',
+  name
 ) {
   const cal = {
     event: {
-      title: 'Register domain',
-      description: 'This is the sample event provided as an example only',
-      location: 'Portland, OR',
+      title: `Renew your ENS domain ${name}`,
+      description: 'Your ENS name is expiring soon, please renew it',
+      location: 'Everywhere',
       startTime,
       endTime
     }
   }
-  return <AddToCalendar event={cal} />
+  return <AddToCalendar event={cal.event} />
 }
 
 export default CalendarInvite
