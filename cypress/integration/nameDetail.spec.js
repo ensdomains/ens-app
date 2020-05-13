3d68a69b955af48d299c5372a43b8f7bc04fce11
Makoto <inouemak@googlemail.com>
2020-05-13 20:11:46 +0100

    Add timeout where possible

--- cypress/integration/nameDetail.spec.js
+++ cypress/integration/nameDetail.spec.js
@@ -44,10 +44,14 @@ describe('Name detail view', () => {
     cy.getByText('Transfer').click({ force: true })
 
     cy.getByTestId('name-details').within(container => {
-      cy.getByPlaceholderText('address', { container, exact: false }).type(
-        '0x0000000000000000000000000000000000000001',
-        { force: true, delay: 0 }
-      )
+      cy.getByPlaceholderText('address', {
+        container,
+        exact: false,
+        timeout: 10000
+      }).type('0x0000000000000000000000000000000000000001', {
+        force: true,
+        delay: 0
+      })
     })
     waitUntilInputResolves('Transfer').then(() => {
       cy.getByText('Transfer').click({ force: true })
@@ -68,10 +72,11 @@ describe('Name detail view', () => {
       () => {
         cy.getByTestId('edit-resolver').click({ force: true })
         cy.getByTestId('name-details').within(container => {
-          cy.getByPlaceholderText('address', { container, exact: false }).type(
-            '0x0000000000000000000000000000000000000002',
-            { force: true }
-          )
+          cy.getByPlaceholderText('address', {
+            container,
+            timeout: 10000,
+            exact: false
+          }).type('0x0000000000000000000000000000000000000002', { force: true })
         })
         waitUntilInputResolves('Save').then(() => {
           cy.getByText('Save').click({ force: true })
@@ -92,7 +97,7 @@ describe('Name detail view', () => {
 
         cy.getByText('Use Public Resolver', {
           exact: true,
-          timeout: 5000
+          timeout: 10000
         }).click({
           force: true
         })
@@ -101,11 +106,23 @@ describe('Name detail view', () => {
           cy.getByTestId('name-details').within(container => {
             cy.getByPlaceholderText('address', {
               container,
+              timeout: 10000,
               exact: false
             }).then($address => {
               cy.getByText('Save', { timeout: 5000 }).click({ force: true })
-              cy.getByText($address.val(), { timeout: 10000 }).should('exist')
             })
+          })
+        })
+        waitUntilInputResolves('Confirm').then(() => {
+          cy.getByText('Confirm', { timeout: 5000 }).click({ force: true })
+        })
+        cy.getByTestId('name-details').within(container => {
+          cy.getByPlaceholderText('address', {
+            container,
+            timeout: 10000,
+            exact: false
+          }).then($address => {
+            cy.getByText($address.val(), { timeout: 10000 }).should('exist')
           })
         })
       }
@@ -117,11 +134,14 @@ describe('Name detail view', () => {
 
     cy.getByTestId('name-details').within(container => {
       cy.getByText('+').click({ force: true })
-      cy.getByText('select a record', { exact: false }).click({ force: true })
+      cy.getByText('select a record', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.get('#react-select-2-option-0')
         .contains('Address')
         .click({ force: true })
       cy.getByPlaceholderText('Enter Ethereum name or address', {
+        timeout: 10000,
         exact: false
       }).type('blah', { force: true })
 
@@ -151,7 +171,9 @@ describe('Name detail view', () => {
 
     cy.getByTestId('name-details').within(container => {
       cy.getByText('+').click({ force: true })
-      cy.getByText('select a record', { exact: false }).click({ force: true })
+      cy.getByText('select a record', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.get('#react-select-2-option-0')
         .contains('Address')
         .click({ force: true })
@@ -179,7 +201,7 @@ describe('Name detail view', () => {
     cy.getByTestId('name-details').within(container => {
       cy.getByText('+')
         .click({ force: true })
-        .getByText('select a record', { exact: false })
+        .getByText('select a record', { exact: false, timeout: 10000 })
         .click({ force: true })
         .get('#react-select-2-option-1', { timeout: 10000 })
         .contains('Content')
@@ -206,7 +228,7 @@ describe('Name detail view', () => {
     cy.getByTestId('name-details', { timeout: 10000 }).within(container => {
       cy.getByText('+')
         .click({ force: true })
-        .getByText('select a record', { exact: false })
+        .getByText('select a record', { exact: false, timeout: 10000 })
         .click({ force: true })
         .getByText('Other addresses')
         .click({ force: true })
@@ -214,7 +236,10 @@ describe('Name detail view', () => {
         .click({ force: true })
         .getByText('LTC', { exact: false })
         .click({ force: true })
-        .getByPlaceholderText('Enter a LTC address', { exact: false })
+        .getByPlaceholderText('Enter a LTC address', {
+          exact: false,
+          timeout: 10000
+        })
         .type(address, { force: true })
       waitUntilInputResolves('Save').then(() => {
         cy.getByText('Save').click({ force: true })
@@ -233,7 +258,7 @@ describe('Name detail view', () => {
     cy.getByTestId('name-details', { timeout: 10000 }).within(container => {
       cy.getByText('+')
         .click({ force: true })
-        .getByText('select a record', { exact: false })
+        .getByText('select a record', { exact: false, timeout: 10000 })
         .click({ force: true })
         .getByText('Text')
         .click({ force: true })
@@ -241,7 +266,7 @@ describe('Name detail view', () => {
         .click({ force: true })
         .getByText('Notice', { exact: false })
         .click({ force: true })
-        .getByPlaceholderText('Enter notice', { exact: false })
+        .getByPlaceholderText('Enter notice', { exact: false, timeout: 10000 })
         .type(text, { force: true })
       waitUntilInputResolves('Save').then(() => {
         cy.getByText('Save').click({ force: true })
@@ -288,6 +313,7 @@ describe('Name detail view', () => {
     cy.getByTestId('name-details').within(container => {
       cy.getByTestId('edit-address', { exact: false }).click({ force: true })
       cy.getByPlaceholderText('Enter Ethereum name or address', {
+        timeout: 10000,
         exact: false
       }).type(ADDRESS, { force: true })
 
@@ -314,8 +340,11 @@ describe('Name detail view', () => {
     cy.visit(`${NAME_ROOT}/abittooawesome.eth`)
 
     cy.getByTestId('name-details').within(container => {
-      cy.getByTestId('edit-content', { exact: false }).click({ force: true })
+      cy.getByTestId('edit-content', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.getByPlaceholderText('Enter a content hash', {
+        timeout: 10000,
         exact: false
       }).type(content, { force: true })
 
@@ -337,8 +366,11 @@ describe('Name detail view', () => {
     cy.visit(`${NAME_ROOT}/notsoawesome.eth`)
 
     cy.getByTestId('name-details').within(container => {
-      cy.getByTestId('edit-notice', { exact: false }).click({ force: true })
+      cy.getByTestId('edit-notice', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.getByPlaceholderText('Enter notice', {
+        timeout: 10000,
         exact: false
       }).type(content, { force: true })
 
@@ -361,8 +393,11 @@ describe('Name detail view', () => {
     cy.visit(`${NAME_ROOT}/notsoawesome.eth`)
 
     cy.getByTestId('name-details').within(container => {
-      cy.getByTestId('edit-ltc', { exact: false }).click({ force: true })
+      cy.getByTestId('edit-ltc', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.getByPlaceholderText('Enter a LTC address', {
+        timeout: 10000,
         exact: false
       }).type(content, { force: true })
 
@@ -374,7 +409,9 @@ describe('Name detail view', () => {
         waitUntilTestIdDoesNotExist('cancel')
 
         //Value updated
-        cy.queryByText(content, { exact: false }).should('exist')
+        cy.queryByText(content, { exact: false, timeout: 10000 }).should(
+          'exist'
+        )
       })
     })
   })
@@ -382,20 +419,38 @@ describe('Name detail view', () => {
   it('can delete records', () => {
     cy.visit(`${NAME_ROOT}/notsoawesome.eth`)
     cy.getByTestId('name-details').within(container => {
-      cy.getByTestId('edit-address', { exact: false }).click({ force: true })
-      cy.getByTestId('delete-address', { exact: false }).click({ force: true })
+      cy.getByTestId('edit-address', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
+      cy.getByTestId('delete-address', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
       cy.wait(1000)
 
-      cy.getByTestId('edit-content', { exact: false }).click({ force: true })
-      cy.getByTestId('delete-content', { exact: false }).click({ force: true })
-
-      cy.getByTestId('edit-ltc', { exact: false }).click({ force: true })
-      cy.getByTestId('delete-KeyValue-ltc', { exact: false }).click({
+      cy.getByTestId('edit-content', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
+      cy.getByTestId('delete-content', { exact: false, timeout: 10000 }).click({
         force: true
       })
 
-      cy.getByTestId('edit-notice', { exact: false }).click({ force: true })
-      cy.getByTestId('delete-KeyValue-notice', { exact: false }).click({
+      cy.getByTestId('edit-ltc', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
+      cy.getByTestId('delete-KeyValue-ltc', {
+        exact: false,
+        timeout: 10000
+      }).click({
+        force: true
+      })
+
+      cy.getByTestId('edit-notice', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
+      cy.getByTestId('delete-KeyValue-notice', {
+        exact: false,
+        timeout: 10000
+      }).click({
         force: true
       })
       cy.wait(1000)
@@ -407,7 +462,7 @@ describe('Name detail view', () => {
 
   it('can navigate to a subdomain', () => {
     cy.visit(`${NAME_ROOT}/subdomaindummy.eth`)
-      .getByText('subdomains', { exact: false })
+      .getByText('subdomains', { exact: false, timeout: 10000 })
       .click({ force: true })
       .getByText('original.subdomaindummy.eth', { timeout: 10000 })
       .click({ force: true })
@@ -416,20 +471,25 @@ describe('Name detail view', () => {
   it('can add a subdomain', () => {
     const LABEL = 'sub1' // using the same subdomain label which is used at sub1.testing.eth
     cy.visit(`${NAME_ROOT}/subdomaindummy.eth`)
-      .getByText('subdomains', { exact: false })
+      .getByText('subdomains', { exact: false, timeout: 10000 })
       .click({ force: true })
 
     cy.getByTestId('subdomains').within(() => {
       cy.wait(1000)
-      cy.getByText('add', { exact: false }).click({ force: true })
-      cy.getByPlaceholderText('Type in a label', { exact: false }).type(LABEL, {
+      cy.getByText('add', { exact: false, timeout: 10000 }).click({
+        force: true
+      })
+      cy.getByPlaceholderText('Type in a label', {
+        exact: false,
+        timeout: 10000
+      }).type(LABEL, {
         force: true
       })
       cy.getByText('save', { exact: false }).click({ force: true })
     })
     cy.wait(1000)
     cy.visit(`${NAME_ROOT}/subdomaindummy.eth/subdomains`)
-      .getByText('subdomains', { exact: false })
+      .getByText('subdomains', { exact: false, timeout: 10000 })
       .click({ force: true })
 
     cy.queryByText(`${LABEL}.subdomaindummy.eth`, { timeout: 10000 }).should(
