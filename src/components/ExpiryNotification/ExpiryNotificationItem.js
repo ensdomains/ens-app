3dbb65503b7eafd69b936eeb90623fda1da7e8cf
Mike Powers <mjpowersjr@gmail.com>
2020-05-20 15:53:45 -0400

    Improved styling of email input view

--- src/components/ExpiryNotification/ExpiryNotificationItem.js
+++ src/components/ExpiryNotification/ExpiryNotificationItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 import { useTranslation } from 'react-i18next'
-//import mq from 'mediaQuery'
+import mq from 'mediaQuery'
 
 import {
   DetailsItem,
@@ -19,7 +19,32 @@ import Loader from '../Loader'
 
 import { EmailComponent } from '@buidlhub/buidlhub-ens-notifications'
 
-const EmailNotifyButton = styled(Button)``
+const EmailNotifyButton = styled(Button)`
+  margin-right: 20px;
+  align-self: flex-start;
+  width: 200px;
+`
+
+const ExpiryNotificationContainer = styled(DetailsItem)`
+  grid-column: 1/3;
+  background: ${({ editing, backgroundStyle }) => {
+    switch (backgroundStyle) {
+      case 'warning':
+        return editing ? 'transparent' : 'transparent'
+      default:
+        return editing ? '#F0F6FA' : 'transparent'
+    }
+  }};
+  padding: ${({ editing }) => (editing ? '20px' : '0')};
+  ${({ editing }) => (editing ? `margin-bottom: 20px;` : '')}
+  transition: 0.3s;
+
+  ${({ editing }) => editing && mq.small` flex-direction: column;`};
+
+  > div {
+    width: 100%;
+  }
+`
 
 const ExpiryNotificationItemEditable = ({ address }) => {
   const { t, i18n } = useTranslation()
@@ -31,14 +56,29 @@ const ExpiryNotificationItemEditable = ({ address }) => {
 
   const loading = <Loader />
 
+  const Buttons = styled('div')`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-wrap: wrap;
+  `
+
   const submitStyle = [
     getButtonDefaultStyles(),
-    getButtonStyles({ type: 'primary' })
+    getButtonStyles({ type: 'primary' }),
+    `
+    position: relative;
+    float: right;
+    `
   ].join('\n')
 
   const cancelStyle = [
     getButtonDefaultStyles(),
-    getButtonStyles({ type: 'hollow' })
+    getButtonStyles({ type: 'hollow' }),
+    `  
+    position: relative;
+    float: right;
+    `
   ].join('\n')
 
   return (
@@ -50,12 +90,12 @@ const ExpiryNotificationItemEditable = ({ address }) => {
       )}
 
       {editing && (
-        <>
+        <ExpiryNotificationContainer backgroundStyle="blue" editing={editing}>
           <EmailComponent
             labelContainer={DetailsItem}
-            actionsContainer={DetailsContent}
+            actionsContainer={Buttons}
             messageContainer={DetailsKey}
-            emailInputStyle="width: 100%;"
+            emailInputStyle="width: 100%; margin: 1em 0;"
             onCancel={stopEditing}
             cancelStyle={cancelStyle}
             language={i18n.lng}
@@ -63,7 +103,7 @@ const ExpiryNotificationItemEditable = ({ address }) => {
             publicAddress={address}
             submitStyle={submitStyle}
           />
-        </>
+        </ExpiryNotificationContainer>
       )}
     </>
   )
