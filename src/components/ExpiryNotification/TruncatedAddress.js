52095baff903cf64d7fa1d9a5a6c76510a3046fe
Mike Powers <mjpowersjr@gmail.com>
2020-05-27 10:20:27 -0400

    Replaced inline css w. styled component

--- src/components/ExpiryNotification/TruncatedAddress.js
+++ src/components/ExpiryNotification/TruncatedAddress.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const TruncatedAddressContainer = styled('span')`
+  font-weight: 500;
+`
 
 export default function TruncatedAddress({ address = '' }) {
   const showFullAddress = false
   const addressTruncated = address.substr(0, 14)
 
   return (
-    <span title={address} style={{ fontWeight: 500 }}>
+    <TruncatedAddressContainer title={address}>
       {showFullAddress ? address : addressTruncated}
-
       {!showFullAddress && <span>&hellip;</span>}
-    </span>
+    </TruncatedAddressContainer>
   )
 }
