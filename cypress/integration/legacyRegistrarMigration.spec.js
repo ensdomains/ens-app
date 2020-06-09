c32a41e4b695d84ccd2006f06e353d0a2a1bf344
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-06-09 18:28:08 +0100

    Make Return button unclickable if not owner (#806)

--- cypress/integration/legacyRegistrarMigration.spec.js
+++ cypress/integration/legacyRegistrarMigration.spec.js
@@ -4,11 +4,38 @@ const NAME_ROOT = Cypress.env('NAME_ROOT')
 describe('Release Deed from Migrated Legacy registrar', () => {
   it('can visit a name and migrate it', () => {
     cy.visit(`${ROOT}/name/auctioned3.eth`)
+    cy.queryByText(
+      'Your name was automatically migrated to the new Registrar',
+      {
+        timeout: 5000,
+        exact: false
+      }
+    ).should('exist')
+
+    cy.queryByTestId('enabled-return-button', {
+      exact: false,
+      timeout: 1000
+    }).should('exist')
     cy.getByText('Return').click({ force: true })
     cy.queryByText('Your deposit is now returned.', {
       timeout: 5000,
       exact: false
     }).should('exist')
     cy.queryByText('Return', { timeout: 50 }).should('not.exist')
+  })
+
+  it('cannot press return button of name not owned by you', () => {
+    cy.visit(`${ROOT}/name/auctionedbysomeoneelse.eth`)
+    cy.queryByText(
+      'This name was automatically migrated to the new Registrar',
+      {
+        timeout: 5000,
+        exact: false
+      }
+    ).should('exist')
+    cy.queryByTestId('disabled-return-button', {
+      exact: false,
+      timeout: 1000
+    }).should('exist')
   })
 })
