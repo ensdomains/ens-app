297fb120dfbedb884f73e8fd4547c55a040d7ee6
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-07-07 18:09:15 +0100

    Add isUnderPremiumSale

--- src/components/DomainItem/ExpiryDate.js
+++ src/components/DomainItem/ExpiryDate.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import styled from '@emotion/styled/macro'
 import { useTranslation } from 'react-i18next'
-import { formatDate, calculateIsExpiredSoon, GRACE_PERIOD } from 'utils/dates'
+import {
+  formatDate,
+  calculateIsExpiredSoon,
+  GRACE_PERIOD,
+  PREMIUM_PERIOD
+} from 'utils/dates'
 
 const ExpiryDateContainer = styled('p')`
   font-size: 18px;
@@ -10,12 +15,38 @@ const ExpiryDateContainer = styled('p')`
 `
 
 const ExpiryDate = ({ expiryDate, domain, name }) => {
-  let isExpiredSoon, isExpired, gracePeriodEndDate
+  let isExpiredSoon,
+    isExpired,
+    gracePeriodEndDate,
+    isUnderPremiumSale,
+    isInGracePeriod,
+    endOfPremiumDate,
+    message
   let { t } = useTranslation()
+  const now = new Date()
   if (expiryDate) {
     isExpiredSoon = calculateIsExpiredSoon(expiryDate)
-    isExpired = new Date() > new Date(parseInt(expiryDate * 1000))
+    isExpired = now > new Date(parseInt(expiryDate * 1000))
     gracePeriodEndDate = new Date((parseInt(expiryDate) + GRACE_PERIOD) * 1000)
+    endOfPremiumDate = new Date(
+      (parseInt(expiryDate) + GRACE_PERIOD + PREMIUM_PERIOD) * 1000
+    )
+    if (isExpired) {
+      isInGracePeriod = now < gracePeriodEndDate
+      isUnderPremiumSale = !isInGracePeriod && now < endOfPremiumDate
+      if (isInGracePeriod) {
+        message = `${t('singleName.expiry.gracePeriodEnds')} ${formatDate(
+          gracePeriodEndDate
+        )}`
+      } else if (isUnderPremiumSale) {
+        message = t('singleName.expiry.isUnderPremiumSale')
+      } else {
+        // sale under normal price
+      }
+    } else {
+      // not expired
+      message = `${t('c.expires')} ${formatDate(parseInt(expiryDate * 1000))}`
+    }
   } else {
     return <span>&nbsp;</span>
   }
@@ -24,11 +55,7 @@ const ExpiryDate = ({ expiryDate, domain, name }) => {
       data-testid={`expiry-date-${name}`}
       isExpiredSoon={isExpiredSoon}
     >
-      {isExpired
-        ? `${t('singleName.expiry.gracePeriodEnds')} ${formatDate(
-            gracePeriodEndDate
-          )}`
-        : `${t('c.expires')} ${formatDate(parseInt(expiryDate * 1000))}`}
+      {message}
     </ExpiryDateContainer>
   )
 }
