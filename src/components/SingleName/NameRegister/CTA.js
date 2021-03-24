de30b218427bea80800487319f7789b0013857aa
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-03-24 17:41:11 +0000

    Call trackReferral when transaction is complete

--- src/components/SingleName/NameRegister/CTA.js
+++ src/components/SingleName/NameRegister/CTA.js
@@ -141,18 +141,6 @@ function getCTA({
         onCompleted={data => {
           const txHash = Object.values(data)[0]
           setTxHash(txHash)
-          if (ethUsdPrice) {
-            // this is not set on local test env
-            trackReferral({
-              transactionId: txHash,
-              labels: [label],
-              type: 'register', // renew/register
-              price: new EthVal(`${price._hex}`)
-                .toEth()
-                .mul(ethUsdPrice)
-                .toFixed(2) // in wei, // in wei
-            })
-          }
           incrementStep()
         }}
       >
@@ -187,6 +175,18 @@ function getCTA({
       <PendingTx
         txHash={txHash}
         onConfirmed={async () => {
+          if (ethUsdPrice) {
+            // this is not set on local test env
+            trackReferral({
+              transactionId: txHash,
+              labels: [label],
+              type: 'register', // renew/register
+              price: new EthVal(`${price._hex}`)
+                .toEth()
+                .mul(ethUsdPrice)
+                .toFixed(2) // in wei, // in wei
+            })
+          }
           incrementStep()
         }}
       />
