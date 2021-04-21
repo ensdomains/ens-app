b7c3b682e2a35e2183a3edce0b52ccf443234bb2
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-04-21 18:03:25 +0100

    Add 300

--- src/components/Address/Pager.js
+++ src/components/Address/Pager.js
@@ -61,7 +61,8 @@ const options = [
   { value: 25, label: 25 },
   { value: 50, label: 50 },
   { value: 100, label: 100 },
-  { value: 200, label: 200 }
+  { value: 200, label: 200 },
+  { value: 300, label: 300 }
 ]
 
 function useTotalPages({
