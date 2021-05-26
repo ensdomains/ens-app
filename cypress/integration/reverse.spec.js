9a5d85d108cd130ec33f7b29f3ec18c9e81a45e0
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-05-26 15:29:54 +0100

    Create test to set reverse record

--- cypress/integration/reverse.spec.js
+++ cypress/integration/reverse.spec.js
@@ -1,25 +1,5 @@
 const ROOT = Cypress.env('ROOT')
-const NAME_ROOT = Cypress.env('NAME_ROOT')
 const ADDRESS = Cypress.env('ownerAddress')
-
-function confirmRecordUpdate() {
-  cy.getByTestId('action').click({ force: true })
-  cy.getByTestId('send-transaction').click({
-    force: true
-  })
-  cy.wait(1000)
-}
-
-function refreshAndCheckText(url, textOrArrayOfText) {
-  cy.visit(url)
-  if (typeof textOrArrayOfText === 'string') {
-    cy.queryByText(textOrArrayOfText, { timeout: 20000 }).should('exist')
-  } else {
-    textOrArrayOfText.forEach(text =>
-      cy.queryByText(text, { timeout: 20000 }).should('exist')
-    )
-  }
-}
 
 describe(
   'Reverse record',
@@ -29,57 +9,40 @@ describe(
     }
   },
   () => {
-    it.only('is set to abittooawesome2.eth', () => {
-      const name = 'abittooawesome2.eth'
-      const url = `${NAME_ROOT}/${name}`
+    it('is set', () => {
+      const url = `${ROOT}/address/${ADDRESS}`
       cy.visit(url)
+      cy.queryByText(`everse record: not set`, {
+        exact: false,
+        timeout: 10000
+      }).should('exist')
 
-      // cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
-      //   'not.have.text',
-      //   name
-      // )
-
-      // cy.getByTestId('name-details', { timeout: 10000 }).within(container => {
-      //   cy.getByText('Add/Edit Record').click({ force: true })
-      //   cy.wait(2000)
-      //   // Address
-      //   cy.getByTestId('ETH-record-input')
-      //     .clear({ force: true })
-      //     .type(ADDRESS, { force: true })
-      // })
-
-      // confirmRecordUpdate()
-      // refreshAndCheckText(url, [ADDRESS])
-
-      cy.getByText('My Account').click({ force: true })
-
-      cy.wait(1500)
-      cy.queryByTestId('editable-reverse-record-set', { exact: false }).should(
-        'exist'
-      )
       cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
-        'not.have.text',
-        name
+        'have.text',
+        ADDRESS
       )
       cy.getByText('Select your ENS name', { exact: false })
         .click({ force: true })
         .get('#react-select-2-option-1', { timeout: 10000 })
-        .contains(name)
-        .click({ force: true })
-      cy.queryByText(name, { exact: false })
-        .click({ force: true })
-        .getByText('Save', { timeout: 5000 })
-        .click({ force: true })
+        .invoke('text')
+        .then(name => {
+          console.log('***TEST', { name })
+          cy.get('#react-select-2-option-1', { timeout: 10000 })
+            .click({ force: true })
+            .getByText('Save', { timeout: 5000 })
+            .click({ force: true })
 
-      cy.queryByText(`Reverse record: Set to ${name}`, {
-        exact: false,
-        timeout: 10000
-      }).should('exist')
-      cy.getByText('My Account').click({ force: true })
-      cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
-        'not.have.text',
-        name
-      )
+          cy.queryByText(`Reverse record: Set to`, {
+            exact: false,
+            timeout: 10000
+          }).should('exist')
+          cy.visit(url)
+          cy.wait(5000)
+          cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
+            'have.text',
+            name
+          )
+        })
     })
   }
 )
