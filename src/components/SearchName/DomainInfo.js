6bca7a0334ef01406692dc498c7d23e6179c099e
Leon Talbert <leon.talbert@gmail.com>
2021-08-13 12:05:20 +0800

    domain info fix

--- src/components/SearchName/DomainInfo.js
+++ src/components/SearchName/DomainInfo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Query } from 'react-apollo'
+import { useQuery } from '@apollo/client'
 import DomainItem from '../DomainItem/DomainItem'
 import { GET_FAVOURITES, GET_SINGLE_NAME } from '../../graphql/queries'
 
@@ -13,36 +14,42 @@ export const DomainInfo = ({ domainState, isFavourite, loading }) => {
   )
 }
 
-const DomainInfoContainer = ({ searchTerm }) => {
-  return (
-    <Query query={GET_SINGLE_NAME} variables={{ name: searchTerm }}>
-      {({ data, loading, error }) => {
-        if (loading || !data) return null
-        if (error) {
-          console.error(error)
-          return null
-        }
-        const { singleName } = data
+const DomainItemContainer = ({ singleName, searchTerm }) => {
+  const {
+    data: { favourites },
+    loading
+  } = useQuery(GET_FAVOURITES, {
+    name: searchTerm
+  })
 
-        return (
-          <Query query={GET_FAVOURITES}>
-            {({ data: { favourites } = {} }) => (
-              <DomainItem
-                loading={loading}
-                domain={singleName}
-                isFavourite={
-                  singleName &&
-                  favourites &&
-                  favourites.filter(domain => domain.name === singleName.name)
-                    .length > 0
-                }
-              />
-            )}
-          </Query>
-        )
-      }}
-    </Query>
+  return (
+    <DomainItem
+      loading={loading}
+      domain={singleName}
+      isFavourite={
+        singleName &&
+        favourites &&
+        favourites.filter(domain => domain.name === singleName.name).length > 0
+      }
+    />
   )
+}
+
+const DomainInfoContainer = ({ searchTerm }) => {
+  const { data, loading, error } = useQuery(GET_SINGLE_NAME, {
+    variables: {
+      name: searchTerm
+    }
+  })
+
+  if (loading || !data) return null
+  if (error) {
+    console.error(error)
+    return null
+  }
+  const { singleName } = data
+
+  return <DomainItemContainer {...{ singleName, searchTerm }} />
 }
 
 export default DomainInfoContainer
