c8d0effad3069137ec41cc3c5ee8e43e681fdc6e
Leon Talbert <leon.talbert@gmail.com>
2021-09-09 14:20:21 +0800

    reverse spec fix

--- cypress/integration/reverse.spec.js
+++ cypress/integration/reverse.spec.js
@@ -12,39 +12,40 @@ describe(
     it('is set', () => {
       const url = `${ROOT}/address/${ADDRESS}`
       cy.visit(url)
+      cy.wait(5000)
+
       cy.queryByText(`everse record: not set`, {
         exact: false,
         timeout: 10000
       })
-
-      cy.wait(10000)
 
       cy.getByTestId('account', { exact: false, timeout: 20000 }).should(
         'have.text',
         `${ADDRESS.slice(0, 10)}...`
       )
 
-      cy.getByText('Select your ENS name', { exact: false })
-        .click({ force: true })
-        .get('#react-select-3-option-1', { timeout: 10000 })
-        .invoke('text')
-        .then(name => {
-          cy.get('#react-select-3-option-1', { timeout: 10000 })
-            .click({ force: true })
-            .getByText('Save', { timeout: 5000 })
-            .click({ force: true })
+      cy.getByText('Select your ENS name', { exact: false }).click({
+        force: true
+      })
 
-          cy.queryByText(`Reverse record: Set to`, {
-            exact: false,
-            timeout: 10000
-          }).should('exist')
-          cy.visit(url)
-          cy.wait(5000)
-          cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
-            'have.text',
-            name
-          )
-        })
+      cy.getByText('sub1.otherowner.eth', { exact: true }).click({
+        force: true
+      })
+
+      cy.getByText('Save', { timeout: 5000 }).click({ force: true })
+
+      cy.queryByText(`Reverse record: Set to`, {
+        exact: false,
+        timeout: 10000
+      }).should('exist')
+
+      cy.visit(url)
+      cy.wait(5000)
+
+      cy.getByTestId('account', { exact: false, timeout: 10000 }).should(
+        'have.text',
+        'sub1.otherowner.eth'
+      )
     })
   }
 )
