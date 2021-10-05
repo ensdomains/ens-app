c94a2a5672b13963ac662eb40fad7555525da92b
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2021-10-05 20:33:12 +0300

    recognize avatar ipfs hash if protocol not present

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -2,6 +2,7 @@ import { useEffect, useReducer, useRef, useState } from 'react'
 import { utils } from 'ethers'
 import getEtherPrice from 'api/price'
 import { useLocation } from 'react-router-dom'
+import { cid as isCID } from 'is-ipfs'
 import { loggedIn, logout } from './IPFS/auth'
 import { getBlock, getProvider } from '@ensdomains/ui'
 import { networkName, supportedAvatarProtocols } from 'utils/utils'
@@ -233,8 +234,12 @@ export function useAvatar(textKey, name, network, uri) {
           `https://metadata.ens.domains/${_network}/avatar/${name}/meta`
         )
         const data = await result.json()
-        if ('image' in data && protocol === 'ipfs://') {
-          data.image = data.image.replace('ipfs://', 'https://ipfs.io/ipfs/')
+        if ('image' in data) {
+          if (protocol === 'ipfs://') {
+            data.image = data.image.replace('ipfs://', 'https://ipfs.io/ipfs/')
+          } else if (isCID(data.image)) {
+            data.image = `https://ipfs.io/ipfs/${data.image}`
+          }
         }
         setAvatar(data)
       }
