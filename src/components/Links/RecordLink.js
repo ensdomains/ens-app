f8211462536abf6da80baccc61d908582bc2f003
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-10-29 14:54:03 +0100

    Added new records

--- src/components/Links/RecordLink.js
+++ src/components/Links/RecordLink.js
@@ -63,6 +63,7 @@ const RecordLink = ({ textKey, value, name }) => {
   let url
   const { network } = useNetworkInfo()
   switch (textKey) {
+    case 'eth.ens.delegate':
     case 'url':
       url = `${value}`
       break
