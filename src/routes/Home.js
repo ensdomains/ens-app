1d3d5945781b7de164281ae4a3315ba063411fd4
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-11-01 16:35:25 +0000

    Reuse component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,11 +13,13 @@ import TextBubbleDefault from '../components/Icons/TextBubble'
 import QuestionMarkDefault from '../components/Icons/QuestionMark'
 import HowToUseDefault from '../components/HowToUse/HowToUse'
 import ENSLogo from '../components/HomePage/images/ENSLogo.svg'
-import ENSIcon from '../components/HomePage/images/ENSIcon.svg'
-import Arrow from '../components/HomePage/images/Arrow.svg'
 import { aboutPageURL } from '../utils/utils'
 import { connectProvider, disconnectProvider } from '../utils/providerUtils'
 import gql from 'graphql-tag'
+import {
+  MainPageBannerContainer,
+  DAOBannerContent
+} from '../components/Banner/DAOBanner'
 
 const HeroTop = styled('div')`
   display: grid;
@@ -251,63 +253,6 @@ const LogoLarge = styled(motion.img)`
   `}
 `
 
-const Banner = styled(`div`)`
-  position: absolute;
-  top: 50px;
-  margin-left: auto;
-  margin-right: auto;
-  left: 0;
-  right: 0;
-  background: #ffffff;
-  border-radius: 14px;
-  max-width: 90%;
-  display: grid;
-  padding: 15px 0px;
-  grid-template-columns: 73px 1fr 50px;
-  ${mq.small`
-    width: 600px;
-    height: 78px;
-  `}
-`
-
-const LogoSmall = styled(motion.img)`
-  width: 48px;
-  padding: 10px;
-  border-radius: 50%;
-  margin: auto;
-  display: block;
-  background: linear-gradient(
-    330.4deg,
-    #44bcf0 4.54%,
-    #7298f8 59.2%,
-    #a099ff 148.85%
-  );
-  box-shadow: 0px 4px 26px rgba(0, 0, 0, 0.06);
-`
-
-const ArrowLink = styled(`a`)``
-
-const ArrowSmall = styled(motion.img)`
-  height: 100%;
-  margin: auto;
-  display: block;
-  width: 22px;
-  color: #b3b3b3;
-`
-
-const BannerTitle = styled(`div`)`
-  color: #0e0e0e;
-  font-weight: bold;
-  font-size: 18px;
-`
-
-const BannerContent = styled(`div`)`
-  color: #787878;
-  font-size: 18px;
-  font-weight: 500;
-  font-size: 15px;
-`
-
 const PermanentRegistrarLogo = styled(motion.h1)`
   font-family: Overpass;
   font-weight: 800;
@@ -396,18 +341,9 @@ export default ({ match }) => {
           <NavLink to="/favourites">{t('c.favourites')}</NavLink>
           <ExternalLink href={aboutPageURL()}>{t('c.about')}</ExternalLink>
         </Nav>
-        <Banner>
-          <LogoSmall src={ENSIcon} />
-          <div>
-            <BannerTitle>ENS DAO: A Call for Delegates</BannerTitle>
-            <BannerContent>
-              ENS is decentralizing governance, and we need delegates.
-            </BannerContent>
-          </div>
-          <ArrowLink href="google" com>
-            <ArrowSmall src={Arrow} />
-          </ArrowLink>
-        </Banner>
+        <MainPageBannerContainer>
+          <DAOBannerContent />
+        </MainPageBannerContainer>
       </HeroTop>
       <SearchContainer>
         <>
