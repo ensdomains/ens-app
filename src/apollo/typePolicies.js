7edb48f519dd8ce0ab2d5a6b34b6f342e25425eb
arnaud-eb <arnaud.depierreux@easebest.com>
2021-11-26 15:42:55 +0100

    Update globalError field on Query type

--- src/apollo/typePolicies.js
+++ src/apollo/typePolicies.js
@@ -80,7 +80,12 @@ export default {
       },
       globalError: {
         read() {
-          return globalErrorReactive() || false
+          return (
+            globalErrorReactive() || {
+              network: false,
+              invalidCharacter: false
+            }
+          )
         }
       }
     }
