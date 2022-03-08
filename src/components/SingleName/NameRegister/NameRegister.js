49c596ebea2f3a192584526e57e0c6b1b4fc31af
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-03-08 15:54:26 +0000

    Remove hard coded getPriceCurve

--- src/components/SingleName/NameRegister/NameRegister.js
+++ src/components/SingleName/NameRegister/NameRegister.js
@@ -76,9 +76,7 @@ const NameRegister = ({
     data: { getEthPrice: ethUsdPrice } = {},
     loading: ethUsdPriceLoading
   } = useQuery(GET_ETH_PRICE)
-  // hard coding until oracle.ens.eth is set
-  // const { data: { getPriceCurve } = {} } = useQuery(GET_PRICE_CURVE)
-  const getPriceCurve = 'exponential'
+  const { data: { getPriceCurve } = {} } = useQuery(GET_PRICE_CURVE)
   const { loading: gasPriceLoading, price: gasPrice } = useGasPrice()
   const { block } = useBlock()
   const [invalid, setInvalid] = useState(false)
